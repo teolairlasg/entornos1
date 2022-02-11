df260e43aba3768fbb48de2b42a53bb93367f643
Teo_B01 <teo.lairla@iessierradeguara.com>
2022-02-11 10:49:34 +0100

    añadido símbolo de admiración

--- proyectoRepo1/src/proyectoRepo1/HolaMundo.java
+++ proyectoRepo1/src/proyectoRepo1/HolaMundo.java
@@ -4,7 +4,7 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hola clase");
+		System.out.println("hola clase!");
 	}
 
 }
